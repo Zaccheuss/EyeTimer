ad8d724f8e7a9c36cf89d750df33eecccfe0084f
Zachary <dragonslayerzac@gmail.com>
2020-09-26 21:36:37 -0400

    Create settings fragment

---
+++ app/src/main/java/com/zaccheus/eyetimer/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.zaccheus.eyetimer
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference_screen, rootKey)
+    }
+}
--- app/src/main/java/com/zaccheus/eyetimer/TimerFragment.kt
+++ app/src/main/java/com/zaccheus/eyetimer/TimerFragment.kt
@@ -1,13 +1,13 @@
 package com.zaccheus.eyetimer
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.zaccheus.eyetimer.databinding.FragmentTimerBinding
 
 class TimerFragment : Fragment() {
@@ -19,11 +19,32 @@ class TimerFragment : Fragment() {
     ): View? {
         val binding: FragmentTimerBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_timer, container, false)
+        setHasOptionsMenu(true)
 
         val vm: TimerViewModel by viewModels()
         binding.vm = vm
         binding.lifecycleOwner = this
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_settings, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settings -> {
+                Toast.makeText(context, "Settings", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(TimerFragmentDirections.actionTimerToSettings())
+                return true
+            }
+            R.id.about -> {
+                Toast.makeText(context, "About", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
